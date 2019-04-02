6f7f04954efd4782ebce4c1136160a9d64ba310e
ajafari <ajafari@ajafaris-mbp.lan>
2019-04-01 22:08:35 -0400

    top destinations

--- flights.py
+++ flights.py
@@ -1,80 +1,90 @@
 import unirest
 import pprint
 import sys
+from secrets import *
 
-pp = pprint.PrettyPrinter(indent = 4)
+pp = pprint.PrettyPrinter(indent=4)
 
-# Top 30 US airports. 
+
+# Top 30 US airports.
 def list_of_destinations():
     return ["ATL", "LAX", "ORD", "DFW", "JFK", "SFO", "LAS", "SEA",
-    "CLT", "EWR", "MCO", "PHX", "MIA", "IAH", "BOS", "MSP", "DTW",
-    "PHL", "LGA", "BWI", "SLC", "DCA", "IAD", "SAN", "MDW", "TPA", 
-    "HNL", "PDX", "FLL"]
+            "CLT", "EWR", "MCO", "PHX", "MIA", "IAH", "BOS", "MSP", "DTW",
+            "PHL", "LGA", "BWI", "SLC", "DCA", "IAD", "SAN", "MDW", "TPA",
+            "HNL", "PDX", "FLL"]
+
 
 # API Request from origin to destination
-def make_request(orig, destination, outboundDate, inboundpartialdate):
+def make_request(orig, destination, outbound_date, inbound_partial_date):
     url = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/"
     currency = "USD"
     locale = "en-US"
-    #print("origin is " + orig + "destination is " + destination)
+    # print("origin is " + orig + "destination is " + destination)
 
     response = unirest.get(
-    	url + 
-    	currency + "/" + 
-     	locale +  "/" +
-     	orig + "/" +
-     	destination + "/"  + 
-     	outboundDate + 
-     	"?inboundpartialdate=" + inboundpartialdate ,
-     	headers={
-     	"X-RapidAPI-Key": "a0915c4020mshf5be91092680fb8p158548jsn0ace0d03560a"
-      })
+        url +
+        currency + "/" +
+        locale + "/" +
+        orig + "/" +
+        destination + "/" +
+        outbound_date +
+        "?inboundpartialdate=" + inbound_partial_date,
+        headers={
+            "X-RapidAPI-Key": api_key
+        })
     return response.body
 
+
 # Minimum price of origin to destination
-def cheapest_option(origin, destination, outboundDate, inboundpartialdate):
-    if (origin == destination) :
+def cheapest_option(origin, destination, outbound_date, inbound_partial_date):
+    if origin == destination:
         return 0
-    
-    dic = make_request(origin, destination, outboundDate, inboundpartialdate)
+
+    dic = make_request(origin, destination, outbound_date, inbound_partial_date)
     # There are no flights from origin to this destination
-    if (not dic['Quotes']):
+    if not dic['Quotes']:
         print("flight to " + destination + " does not exist.")
+        # this should be optimized
         return sys.maxsize
 
-    cheapest_option = dic['Quotes'][0]['MinPrice']
-    pp.pprint("Cheapset option from " + origin + " to " +
-        destination + " is: " + str(cheapest_option))
-    return cheapest_option
+    min_price = dic['Quotes'][0]['MinPrice']
+    pp.pprint("Cheapest option from " + origin + " to " +
+              destination + " is: " + str(min_price))
+    return min_price
 
-# Cheapest destination from list of origins. 
-def find_cheapest_destination(origins, outboundDate, inboundpartialdate):
+
+# Cheapest destination from list of origins.
+def find_cheapest_destination(origins, outbound_date, inbound_partial_date):
+    global cheapest_destination
     total_cost = 0
-    total_destination_cost = sys.maxsize # Trying to improve this
+    total_destination_cost = sys.maxsize  # Trying to improve this
+    destinations_to_cost = {} # map of destination and total cost
 
     # Iterate over popular airport destinations
-    for destination in list_of_destinations() :
+    for destination in list_of_destinations():
         # Check cost from given origin
-        for origin in origins:
-            total_cost += cheapest_option(origin, destination, inboundpartialdate, outboundDate)
-            print(total_cost)
-        cheapest_destination
+        for key in origins:
+            total_cost += (origins.get(key) * cheapest_option(key, destination, inbound_partial_date, outbound_date))
+        destinations_to_cost[str(destination)] = total_cost
 
         # Update cheapest option
-        if (total_cost < total_destination_cost) :
+        if total_cost < total_destination_cost:
             print("updating total cost to " + str(total_cost))
             cheapest_destination = destination
             total_destination_cost = total_cost
         total_cost = 0
 
     print("cheapest destination is " + str(cheapest_destination)
-        + " with total cost of " + str(total_destination_cost))
+          + " with total cost of " + str(total_destination_cost))
+
+    sorted_dest = sorted(destinations_to_cost.items(), key=lambda kv: kv[1])
+    for dest in range(3):
+        print(sorted_dest.__getitem__(dest))
+
 
 def main():
-
-    origins = ["BOS", "JFK", "ATL", "JFK"]
+    origins = {"BOS": 1, "JFK": 2, "SEA": 1}
     find_cheapest_destination(origins, "2019-05-06", "2019-05-17")
-    #make_request("JFK", "EWR")
+
 
 main()
-
